fe056c72015b19bfd5e4653d95cba7a616df189b
Fábio Magagnin <fabio.magagnin@meta.com.br>
2018-12-11 21:06:49 -0200

    Até aula 105

---
+++ my-money-app/frontend2/src/main/app.jsx
@@ -0,0 +1,13 @@
+// import '../common/template/dependencies'
+import React from 'react'
+
+// import Header from '../common/template/header'
+// import SideBar from '../common/template/sideBar'
+// import Footer from '../common/template/footer'
+// import Messages from '../common/msg/messages'
+
+export default props => (
+    <div className='wrapper'>
+        <h1>App</h1>
+    </div>
+)
