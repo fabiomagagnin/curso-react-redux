8ce98ab88f493cf8c14c2a0b6ef72a092e99ddef
Fábio Magagnin <fabio.magagnin@meta.com.br>
2019-01-26 16:59:56 -0200

    Até o final da aula 137 (antes do redux-form)

--- my-money-app/frontend2/src/billingCycle/billingCycle.jsx
+++ my-money-app/frontend2/src/billingCycle/billingCycle.jsx
@@ -11,6 +11,7 @@ import TabsContent from '../common/tab/tabsContent'
 import TabHeader from '../common/tab/tabHeader'
 import TabContent from '../common/tab/tabContent'
 import {selectTab, showTabs} from '../common/tab/tabActions'
+import BillingCycleList from './billingCycleList'
 
 class BillingCycle extends Component {
 
@@ -33,7 +34,7 @@ class BillingCycle extends Component {
 
                         </TabsHeader>
                         <TabsContent>
-                            <TabContent id='tabList'><h1>Lista</h1></TabContent>
+                            <TabContent id='tabList'><BillingCycleList/></TabContent>
                             <TabContent id='tabCreate'><h1>Incluir</h1></TabContent>
                             <TabContent id='tabUpdate'><h1>Alterar</h1></TabContent>
                             <TabContent id='tabDelete'><h1>Excluir</h1></TabContent>
