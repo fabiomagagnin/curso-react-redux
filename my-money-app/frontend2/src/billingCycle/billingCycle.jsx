fc919784028cba2ee68d6ca1137f7bf9c937dc33
Fábio Magagnin <fabio.magagnin@meta.com.br>
2019-01-27 16:46:17 -0200

    Até o final da aula 147

--- my-money-app/frontend2/src/billingCycle/billingCycle.jsx
+++ my-money-app/frontend2/src/billingCycle/billingCycle.jsx
@@ -11,7 +11,7 @@ import TabsContent from '../common/tab/tabsContent'
 import TabHeader from '../common/tab/tabHeader'
 import TabContent from '../common/tab/tabContent'
 import {selectTab, showTabs} from '../common/tab/tabActions'
-import {create, update} from './billingCycleActions'
+import {create, update, remove} from './billingCycleActions'
 
 import BillingCycleList from './billingCycleList'
 import BillingCycleForm from './billingCycleForm'
@@ -47,7 +47,9 @@ class BillingCycle extends Component {
                             <TabContent id='tabUpdate'>
                                 <BillingCycleForm onSubmit={this.props.update} />
                             </TabContent>
-                            <TabContent id='tabDelete'><h1>Excluir</h1></TabContent>
+                            <TabContent id='tabDelete'>
+                                <BillingCycleForm onSubmit={this.props.remove} readOnly={true}/>
+                            </TabContent>
 
                         </TabsContent>
                     </Tabs>
@@ -59,6 +61,6 @@ class BillingCycle extends Component {
 }
 
 const mapDispatchToProps = dispatch => bindActionCreators({
-    selectTab, showTabs, create, update}
+    selectTab, showTabs, create, update, remove}
     , dispatch)
 export default connect(null, mapDispatchToProps)(BillingCycle)
