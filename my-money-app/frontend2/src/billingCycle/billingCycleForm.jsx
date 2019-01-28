d044386a33efee469f33f7dbac4cbf2be045c927
Fábio Magagnin <fabio.magagnin@meta.com.br>
2019-01-28 20:54:46 -0200

    Até o final da aula 156

--- my-money-app/frontend2/src/billingCycle/billingCycleForm.jsx
+++ my-money-app/frontend2/src/billingCycle/billingCycleForm.jsx
@@ -6,6 +6,7 @@ import {reduxForm, Field, formValueSelector} from 'redux-form'
 import {init} from './billingCycleActions'
 import LabelAndInput from '../common/form/labelAndInput'
 import ItemList from './itemList'
+import Summary from './summary'
 
 class BillingCycleForm extends Component {
 
@@ -20,6 +21,9 @@ class BillingCycleForm extends Component {
                         label='Mês' cols='12 4' placeholder='Informe o mês'></Field>
                     <Field name='year' component={LabelAndInput} type='number' readOnly={readOnly}
                         label='Ano' cols='12 4' placeholder='Informe o ano'></Field>
+
+                    <Summary credit={1000} debt={100} />
+
 
                     <ItemList cols='12 6' list={credits} readOnly={readOnly} 
                         field='credits' legend='Créditos'/>
